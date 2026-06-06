5d83b047940652c0726b8d7a05689697445e02d4
StephonStyle <stephonaj@gmail.com>
2026-06-06 17:25:34 +0100

    v2.2.36 sw.js缓存版本更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.27';
+const CACHE = '365BET-v2.2.36';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
