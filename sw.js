31ecb8e1d233928ac54edd3657407ab7d0e3ddea
StephonStyle <stephonaj@gmail.com>
2026-06-06 18:30:03 +0100

    v2.2.48 修复结构:替换管理员面板时不破坏tab嵌套

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.47';
+const CACHE = '365BET-v2.2.48';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
