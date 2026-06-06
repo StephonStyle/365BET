3992e8c93965ec92a84d51edae75c1b14f704f09
StephonStyle <stephonaj@gmail.com>
2026-06-06 18:39:22 +0100

    v2.2.49 feat:预测弹窗+赔率生成+管理员面板+try-catch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.48';
+const CACHE = '365BET-v2.2.49';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
