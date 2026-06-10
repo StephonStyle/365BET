1c88d2f5ffe907ea6e70a85752a9e872fcad90ec
StephonStyle <stephonaj@gmail.com>
2026-06-10 13:07:53 +0100

    fix: 预测详情文字调整

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.80';
+const CACHE = '365BET-v2.2.81';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
