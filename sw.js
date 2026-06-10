45a4b77d934af93f1b79952565b8193509e7eb84
StephonStyle <stephonaj@gmail.com>
2026-06-10 22:40:49 +0100

    chore: v2.3.35->v2.3.36, pre-commit hook

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v3.0.0';
+const CACHE = '365BET-v3.0.1';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
