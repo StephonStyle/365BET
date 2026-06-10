1f6e6026ca49cc6114b050d53e11d27dfe8f399a
StephonStyle <stephonaj@gmail.com>
2026-06-10 11:33:40 +0100

    chore: bump v2.3.16→v2.3.17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.71';
+const CACHE = '365BET-v2.2.72';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
