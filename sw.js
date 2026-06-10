569c7b63a3eca6575b615b90e0484a20df17279d
StephonStyle <stephonaj@gmail.com>
2026-06-10 16:56:06 +0100

    chore: v2.3.31->v2.3.32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.86';
+const CACHE = '365BET-v2.2.87';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
