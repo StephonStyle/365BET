80f6d8a365944a4d691aabcb52ee4ffff4709819
StephonStyle <stephonaj@gmail.com>
2026-06-10 13:11:01 +0100

    fix: @前积分数字加粗

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = '365BET-v2.2.81';
+const CACHE = '365BET-v2.2.82';
 self.addEventListener('install',function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(['/365BET/']);}));
